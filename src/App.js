c88d9df38a4e56c6cc6283d08aa9d54d5807ac42
Ricardo <ricardo@brittes.com.br>
2020-05-31 14:28:14 -0300

    Get token

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getToken = async () => {
+    let token = localStorage.getItem("token");
+    if (!token) {
+      const login = await axios.post("http://localhost:3001/users/login", {
+        email: "tuliofaria@devpleno.com",
+        passwd: "abc123",
+      });
+      token = login.data.token;
+      localStorage.setItem("token", token);
+    }
+    const decoded = jwtDecode(token);
+    console.log(decoded);
+
+    const user = await axios.get("http://localhost:3001/users/me", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    console.log(user)
+  };
+  useEffect(() => {
+    getToken();
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
